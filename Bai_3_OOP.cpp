00f7d9fc5bff70d0f00422d76c9ef6fc3d5af35f
23521813 <23521813@gm.uit.edu.vn>
2024-09-29 15:34:49 +0700

    Update Bai_3_OOP.cpp

--- Bai_3_OOP.cpp
+++ Bai_3_OOP.cpp
@@ -1,6 +1,28 @@
+/**
+ *    author:  longvu
+ *    created: 09/29/24 15:34:07
+**/
 #include<bits/stdc++.h>
 
 using namespace std;
+
+#define int long long
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+const int INF = numeric_limits<int>::max();
+const int nax = (int)(100001);
+const int mod = 1e9 + 7;
+
+template<class X, class Y>
+bool maximize(X& x, const Y y) {
+    if (y > x) {x = y; return true;}
+    return false;
+}
+template<class X, class Y>
+bool minimize(X& x, const Y y) {
+    if (y < x) {x = y; return true;}
+    return false;
+}
 
 struct fac { // Phân số
     int p, q;
@@ -23,14 +45,17 @@ fac thuong(fac a, fac b) {
 }
 
 int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     fac a, b;
     cin >> a.p >> a.q >> b.p >> b.q; // Nhập 2 phân số
     fac anstong = tong(a, b)
-        , anshieu = hieu(a, b)
-        , anstich = tich(a, b)
-        , ansthuong = thuong(a, b);
+                  , anshieu = hieu(a, b)
+                              , anstich = tich(a, b)
+                                          , ansthuong = thuong(a, b);
     cout << anstong.p << "/" << anstong.q << '\n'; // Tổng 2 phân số.
     cout << anshieu.p << "/" << anshieu.q << '\n'; // Hiệu 2 phân số.
     cout << anstich.p << "/" << anstich.q << '\n'; // Tích 2 phân số.
     cout << ansthuong.p << "/" << ansthuong.q << '\n'; // Thương 2 phân số.
+    return 0;
 }
